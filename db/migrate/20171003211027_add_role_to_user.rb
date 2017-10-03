0ce5ef83d6d37543af118831d71a972c1154a509
gazzi <gazzi.andres@gmail.com>
2017-10-03 19:37:17 -0300

    first commit

---
+++ db/migrate/20171003211027_add_role_to_user.rb
@@ -0,0 +1,4 @@
+class AddRoleToUser < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Ver el schema para saber que campos tienen los post, creamos primero el usuario porque los post dependen de este
+#Hay que empezar el seed destruyendo los datos
+
+Post.destroy_all
+User.destroy_all
+
+10.times do |i|
+  u = User.create!(name: "User #{i}", email: "Email#{i}@gmail.com", password: "123456")
+  p = Post.create!(title: "Post #{i}", content: "Contenido #{i}", user: u)
+  10.times do |j|
+    c = p.comments.build(content: "comentario #{j} del post #{i}", user: u, post: p)
+    #importante guardar los comentarios para que se vean
+    c.save
+  end
+end
